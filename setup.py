1e50749a128eeb6fb275b88a7769fa250746c715
nishitskunder <51586049+nishitskunder@users.noreply.github.com>
2025-02-11 22:56:06 +0530

    Files uploaded

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MlPipeline",
+    version= "0.0.0",
+    author="Mayank",
+    author_email="mayankpratapsingh022@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
